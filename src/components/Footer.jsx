4589f21e8333bf1ecc87c6f7d5cd557f23ccb283
Riyaz Khan <162485973+rriyazkhan@users.noreply.github.com>
2026-02-20 23:52:58 +0530

    Update contact email and social media links in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,20 +13,14 @@ const Footer = () => {
 				<p className='text-white text-center w-full sm:w-auto font-light'>© {2026} Riyaz khan. All rights reserved.</p>
 				<div className='text-white flex justify-around sm:w-[250px] w-full'>
 					
-					<a href="mailto:karnemahesh19@gmail.com" className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
+					<a href="mailto:rriyazkhan711@gmail.com" className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
 						<FiMail className='text-xl' />
 					</a>
-					<a href="https://x.com/KarneMahes1923" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
-						<FaTwitter className='text-xl' />
-					</a>
-					<a href="https://www.linkedin.com/in/karne-mahesh-6ba583281/" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
+					<a href="https://www.linkedin.com/in/pathan-riyaz-khan/" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
 						<AiFillLinkedin className='text-xl' />
 					</a>
-					<a href="https://github.com/Karnemahesh" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }} >
+					<a href="https://github.com/rriyazkhan" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }} >
 						<FaGithub className='text-xl' />
-					</a>
-					<a href="https://medium.com/@karnemahesh19" target='_blank' className='transition ease-in-out duration-300 rounded-md hover:scale-110 cursor-pointer hover:-translate-y-1' style={{ cursor: 'pointer' }}>
-						<AiFillMediumCircle className='text-xl' />
 					</a>
 				</div>
 			</div>
